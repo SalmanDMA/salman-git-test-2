1cd1241f10d36f82e2ac87cedf320eb8fe257b51
SalmanDMA <salmandwimaulanaakbar96@gmail.com>
2022-11-22 16:43:40 +0700

    menambahkan paragraf baru

--- index.php
+++ index.php
@@ -9,5 +9,6 @@
 <body>
     <h1>hello world!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et iure iste doloribus ratione, laboriosam illo, voluptate hic aspernatur quaerat officiis dolorem autem dolor repudiandae repellendus, consequuntur saepe sed nihil perferendis.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A sequi error, facere reprehenderit obcaecati commodi ea sint? Ex odit, natus sequi cum nihil fuga explicabo! Deleniti nam quaerat fugiat aspernatur!</p>
 </body>
 </html>
